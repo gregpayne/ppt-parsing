2569809477471616c11a5eb3b1709b45ce5d36f1
Greg Payne <gp.greg@gmail.com>
2019-01-27 11:21:52 +0000

    Add static method to get image paths

--- CSharp/ParsePpt/ParsePpt/ParsePpt.cs
+++ CSharp/ParsePpt/ParsePpt/ParsePpt.cs
@@ -54,5 +54,38 @@ namespace ParsePpt
                 return ("Slide " + s + " does not exist in the presentation");
             }
         }
+
+        public static string getImagePath(string path, int s)
+        {
+            try
+            {
+                using (PresentationDocument ppt = PresentationDocument.Open(path, false))
+                {
+                    // Get RelationshipId for first slide
+                    PresentationPart part = ppt.PresentationPart;
+                    OpenXmlElementList slideIds = part.Presentation.SlideIdList.ChildElements;
+                    string relId = (slideIds[s] as SlideId).RelationshipId;
+
+                    // Get the slide part from the RelationshipId
+                    SlidePart slide = (SlidePart)part.GetPartById(relId);
+
+                    // Build StringBuilder object
+                    StringBuilder sb = new StringBuilder();
+
+                    // Get the inner text of the slide
+                    List<ImagePart> imageParts = new List<ImagePart>(slide.ImageParts);
+                    //IEnumerable<ImagePart> images = slide.ImageParts();
+                    foreach (ImagePart image in imageParts)
+                    {
+                        sb.Append(image.Uri + "\n");
+                    }
+                    return sb.ToString();
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return ("Slide " + s + " does not exist in the presentation");
+            }
+        }
     }
 }
