4f62d6ea347a8da856f0ea66569005d78651b7df
Greg Payne <gp.greg@gmail.com>
2019-01-27 10:05:53 +0000

    Add sample C# code to get text from a single slide

---
+++ CSharp/ParsePpt/ParsePpt/ParsePpt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using A = DocumentFormat.OpenXml.Drawing;
+using DocumentFormat.OpenXml.Presentation;
+using B = DocumentFormat.OpenXml.OpenXmlElement;
+
+namespace ParsePpt
+{
+    public class ParsePpt
+    {
+        public static string getSlideText(string path)
+        {
+            using (PresentationDocument ppt = PresentationDocument.Open(path, false))
+            {
+                // Get RelationshipId for first slide
+                PresentationPart part = ppt.PresentationPart;
+                OpenXmlElementList slideIds = part.Presentation.SlideIdList.ChildElements;
+                //foreach (B id in slideIds)
+                //{
+                //    string relId = id as SlideId.Rel
+                //}
+
+                // for debugging, getting slide 1 only
+                string relId = (slideIds[0] as SlideId).RelationshipId;
+
+                // Get the slide part from the RelationshipId
+                SlidePart slide = (SlidePart)part.GetPartById(relId);
+
+                // Build StringBuilder object
+                StringBuilder sb = new StringBuilder();
+
+                // Get the inner text of the slide
+                IEnumerable<A.Text> texts = slide.Slide.Descendants<A.Text>();
+                foreach (A.Text text in texts)
+                {
+                    sb.Append(text.Text);
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
